2826de13911644d55426346ef3e5b56c8a751985
thehoroz <dhr.y.uygun@gmail.com>
2021-05-23 22:03:18 +0200

    Made a score counter, it works, but the reset score function doesn't work yet. Will come back for that later

--- script.js
+++ script.js
@@ -16,23 +16,54 @@ function computerPlay() {
     return selection;
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
+//score works, but can't get the resetScore to work, will figure it out later
+function resetScore(){
+    if (playerScore == 5 || computerScore == 5){
+        playerScore * 0;
+        computerScore * 0;
+    }
+    else {return};
+}
+
+function addPlayerScore() {
+    resetScore();
+    ++playerScore
+};
+
+function addComputerScore(){
+    resetScore();
+    ++computerScore;
+}
+
 function playRound(computerSelection, playerSelection) {
     computerSelection = computerPlay();
+    resetScore();
     if (playerSelection == 'Rock' && computerSelection == 'Paper') {
         alert("Computer chose Paper, you lose!")
+        addComputerScore();
     } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
         alert("Computer chose Scissors, you win!")
+        addPlayerScore()
     } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
         alert("Computer chose Rock, you win!")
+        addPlayerScore()
     } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
         alert("Computer chose Scissors, you lose!")
+        addComputerScore()
     } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
         alert("Computer chose Paper, you win!")
+        addPlayerScore()
     } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
         alert("Computer chose Rock, you lose!")
+        addComputerScore()
     } else {
         alert("Computer chose " + computerSelection + ", it's a draw!")
     }
+    console.log(playerScore);
+    console.log(computerScore);
 };
 
 const rockbtn = document.querySelector('#rockcontainer');
