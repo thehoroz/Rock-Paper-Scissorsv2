0816b6838ac0c0f25e43035096d32c1b4337769d
thehoroz <dhr.y.uygun@gmail.com>
2022-02-07 11:29:28 +0300

    Added the function scoreSetter for less polution

--- script.js
+++ script.js
@@ -22,57 +22,64 @@ function computerPlay() {
     return selection;
 }
 // had to make it into *= rather than just *. It works now.
-function resetScore(){
+
+function scoreSetter() {
     computerScoreboard.textContent = "Computer: " + computerScore;
     playerScoreboard.textContent = "Player: " + playerScore;
+}
+
+function resetScore(){
     if (playerScore == 5){
         alert("You win! Reset the score and play again!")
         playerScore *= 0;
         computerScore *= 0;
-        playerScoreboard.textContent = "Player: " + playerScore;
-        computerScoreboard.textContent = "Computer: " + computerScore;
+        scoreSetter();
         announcement.textContent = "Good job! Now win again!";
 
     } else if (computerScore == 5){
         alert("You lose! Reset the score and play again!")
         playerScore *= 0;
         computerScore *= 0;
-        playerScoreboard.textContent = "Player: " + playerScore;
-        computerScoreboard.textContent = "Computer: " + computerScore;
+        scoreSetter();
         announcement.textContent = "Too bad.. but try again!";
     }
 }
 
 function playRound(computerSelection, playerSelection) {
-    computerSelection = computerPlay();
     if (playerSelection == 'Rock' && computerSelection == 'Paper') {
         announcement.textContent = "Computer chose Paper, you lose!";
         ++computerScore;
+        scoreSetter();
         resetScore();
 
     } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
         announcement.textContent = "Computer chose Scissors, you win!";
         ++playerScore
+        scoreSetter();
         resetScore();
 
     } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
         announcement.textContent = "Computer chose Rock, you win!";
         ++playerScore
+        scoreSetter();
         resetScore();
 
     } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
         announcement.textContent = "Computer chose Scissors, you lose!";
         ++computerScore;
+        scoreSetter();
         resetScore();
 
     } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
         announcement.textContent = "Computer chose Paper, you win!";
         ++playerScore
+        scoreSetter();
         resetScore();
 
     } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
         announcement.textContent = "Computer chose Rock, you lose!";
         ++computerScore;
+        scoreSetter();
         resetScore();
 
     } else {
@@ -84,17 +91,15 @@ function playRound(computerSelection, playerSelection) {
 
 const rockbtn = document.querySelector('#rockcontainer');
 rockbtn.addEventListener('click', () => {
-  playRound('', 'Rock');
+  playRound(computerPlay(), 'Rock');
 });
 
 const paperbtn = document.querySelector('#papercontainer');
 paperbtn.addEventListener('click', () => {
-  playerSelection = 'Paper';
-  playRound('', 'Paper');
+  playRound(computerPlay(), 'Paper');
 });
 
 const scissorbtn = document.querySelector('#scissorcontainer');
 scissorbtn.addEventListener('click', () => {
-  playerSelection = 'Scissors';
-  playRound('', 'Scissors');
+  playRound(computerPlay(), 'Scissors');
 });
